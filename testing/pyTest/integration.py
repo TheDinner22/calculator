23db1bcdb0f26333259d469b726037ffc26e7de0
joe <not_real_email@gmail.com>
2021-09-24 13:01:42 -0400

    added TODO's to remove useless code

--- testing/pyTest/integration.py
+++ testing/pyTest/integration.py
@@ -1,11 +1,5 @@
 integration_tests = {}
 
-# dependencies
-import os, sys
-
-# BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # adds project dir to places it looks for the modules
-# sys.path.append(BASE_PATH)
-
 # example test below
 def one_plus_one_is_two(done):
     outcome = 1+1
